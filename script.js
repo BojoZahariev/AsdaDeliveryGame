de8ddb5c06879671ceba2fec8687c109500793f9
Bojo <bozhidar_zahariev@hotmail.com>
2020-10-21 17:12:46 +0100

    score

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ const jump = () => {
     return;
   }
   character.classList.add('animate');
-  setTimeout(removeJump, 1500);
+  setTimeout(() => {
+    removeJump();
+  }, 1500);
 };
 
 document.addEventListener('click', jump);
@@ -21,12 +23,15 @@ const checkDead = () => {
   let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
   let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
   if (blockLeft < 160 && blockLeft > 50 && characterTop >= 130) {
-    console.log(blockLeft);
     alert('Game over');
+    scoreValue = 0;
   } else if (blockLeft < 10 && blockLeft > 6) {
-    scoreValue += 10;
-    score.textContent = scoreValue;
   }
 };
 
 setInterval(checkDead, 10);
+
+setInterval(() => {
+  scoreValue++;
+  score.textContent = scoreValue;
+}, 300);
