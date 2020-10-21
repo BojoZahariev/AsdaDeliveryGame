967eefa216ed72ceea0e72f86b3c27c85de9096b
Bojo <bozhidar_zahariev@hotmail.com>
2020-10-21 16:52:06 +0100

    score

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const character = document.querySelector('#character');
+const block = document.querySelector('#block');
+const score = document.querySelector('#score');
+let scoreValue = 0;
 
 const jump = () => {
   if (character.classList === 'animate') {
     return;
   }
   character.classList.add('animate');
-  setTimeout(removeJump, 1500); //300ms = length of animation
+  setTimeout(removeJump, 1500);
 };
 
 document.addEventListener('click', jump);
@@ -14,12 +17,15 @@ const removeJump = () => {
   character.classList.remove('animate');
 };
 
-const block = document.querySelector('#block');
 const checkDead = () => {
   let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
   let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
   if (blockLeft < 160 && blockLeft > 50 && characterTop >= 130) {
+    console.log(blockLeft);
     alert('Game over');
+  } else if (blockLeft < 10 && blockLeft > 6) {
+    scoreValue += 10;
+    score.textContent = scoreValue;
   }
 };
 
