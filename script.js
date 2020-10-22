5b744152b0c774d951b468a8fbc97cbcf3caaf18
Bojo <bozhidar_zahariev@hotmail.com>
2020-10-22 14:26:47 +0100

    smoke

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const character = document.querySelector('#character');
 const block = document.querySelector('#block');
 const score = document.querySelector('#score');
+const startBtn = document.querySelector('#startBtn');
 let scoreValue = 0;
 
 const jump = () => {
@@ -13,7 +14,11 @@ const jump = () => {
   }, 1500);
 };
 
-document.addEventListener('click', jump);
+startBtn.addEventListener('click', () => {
+  document.addEventListener('click', jump);
+
+  block.classList.add('animateBlock');
+});
 
 const removeJump = () => {
   character.classList.remove('animate');
@@ -24,6 +29,9 @@ const checkDead = () => {
   let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
   if (blockLeft < 160 && blockLeft > 50 && characterTop >= 130) {
     document.querySelector('#smoke').style.display = 'block';
+    block.classList.remove('animateBlock');
+    block.style.left = `${blockLeft}px`;
+
     //alert('Game over');
     scoreValue = 0;
   } else if (blockLeft < 10 && blockLeft > 6) {
